d1d7490fd01191b9168629a6d0e4ea2785619567
Joey Holliday <joeyholliday77@gmail.com>
2017-01-16 15:56:08 -0600

    Created Project

---
+++ src/com/frcteam1939/steamworks2017/robot/Robot.java
@@ -0,0 +1,62 @@
+
+package com.frcteam1939.steamworks2017.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+public class Robot extends IterativeRobot {
+
+	public static OI oi;
+
+	Command autonomousCommand;
+	SendableChooser<Command> chooser = new SendableChooser<>();
+
+	@Override
+	public void robotInit() {
+		oi = new OI();
+		// chooser.addObject("My Auto", new MyAutoCommand());
+		// SmartDashboard.putData("Auto mode", chooser);
+	}
+
+	@Override
+	public void disabledInit() {
+
+	}
+
+	@Override
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void autonomousInit() {
+		this.autonomousCommand = this.chooser.getSelected();
+
+		if (this.autonomousCommand != null)
+			this.autonomousCommand.start();
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void teleopInit() {
+		if (this.autonomousCommand != null)
+			this.autonomousCommand.cancel();
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
+}
---
+++ src/com/frcteam1939/steamworks2017/robot/RobotMap.java
@@ -0,0 +1,5 @@
+package com.frcteam1939.steamworks2017.robot;
+
+public class RobotMap {
+
+}
