e34f9705443aca1bbac157f93907dc986024b354
azhawa <azhawa@hotmail.com>
2017-01-28 15:35:35 -0600

    Creating Methods for DoubleSolenoid on Sidewinder Module and for Encoders on DriveTrain

--- src/com/frcteam1939/steamworks2017/robot/subsystems/Drivetrain.java
+++ src/com/frcteam1939/steamworks2017/robot/subsystems/Drivetrain.java
@@ -1,10 +1,13 @@
 package com.frcteam1939.steamworks2017.robot.subsystems;
 
 import com.ctre.CANTalon;
+import com.ctre.CANTalon.FeedbackDevice;
 import com.frcteam1939.steamworks2017.robot.RobotMap;
 import com.frcteam1939.steamworks2017.robot.commands.drivetrain.DriveByJoystick;
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -19,24 +22,37 @@ public class Drivetrain extends Subsystem {
 	private CANTalon rightBack = new CANTalon(RobotMap.rightBackTalon);
 	private CANTalon sidewinder = new CANTalon(RobotMap.sidewinderTalon);
 
-	private RobotDrive drive = new RobotDrive(leftFront, rightFront);
+	private RobotDrive drive = new RobotDrive(this.leftFront, this.rightFront);
+
+	private DoubleSolenoid sidewinderModule = new DoubleSolenoid(RobotMap.PCM, RobotMap.sidewinderDownSolenoid,
+			RobotMap.sidewinderUpSolenoid);
+
+	private int frontEncodersCPR = 132;
+	private int sidewinderEncoderCPR = 12;
 
 	private AHRS navx;
 
 	public Drivetrain() {
-		leftFront.setControlMode(CANTalon.TalonControlMode.PercentVbus.value);
-		leftMid.setControlMode(CANTalon.TalonControlMode.Follower.value);
-		leftMid.set(RobotMap.leftFrontTalon);
-		leftBack.setControlMode(CANTalon.TalonControlMode.Follower.value);
-		leftBack.set(RobotMap.leftFrontTalon);
+		this.leftFront.setControlMode(CANTalon.TalonControlMode.PercentVbus.value);
+		this.leftMid.setControlMode(CANTalon.TalonControlMode.Follower.value);
+		this.leftMid.set(RobotMap.leftFrontTalon);
+		this.leftBack.setControlMode(CANTalon.TalonControlMode.Follower.value);
+		this.leftBack.set(RobotMap.leftFrontTalon);
 
-		rightFront.setControlMode(CANTalon.TalonControlMode.PercentVbus.value);
-		rightMid.setControlMode(CANTalon.TalonControlMode.Follower.value);
-		rightMid.set(RobotMap.rightFrontTalon);
-		rightBack.setControlMode(CANTalon.TalonControlMode.Follower.value);
-		rightBack.set(RobotMap.leftFrontTalon);
+		this.rightFront.setControlMode(CANTalon.TalonControlMode.PercentVbus.value);
+		this.rightMid.setControlMode(CANTalon.TalonControlMode.Follower.value);
+		this.rightMid.set(RobotMap.rightFrontTalon);
+		this.rightBack.setControlMode(CANTalon.TalonControlMode.Follower.value);
+		this.rightBack.set(RobotMap.leftFrontTalon);
 
-		sidewinder.setControlMode(CANTalon.TalonControlMode.PercentVbus.value);
+		this.sidewinder.setControlMode(CANTalon.TalonControlMode.PercentVbus.value);
+
+		this.leftFront.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		this.leftFront.configEncoderCodesPerRev(this.frontEncodersCPR);
+		this.rightFront.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		this.rightFront.configEncoderCodesPerRev(this.frontEncodersCPR);
+		this.sidewinder.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		this.sidewinder.configEncoderCodesPerRev(this.sidewinderEncoderCPR);
 
 		try {
 			this.navx = new AHRS(SerialPort.Port.kMXP);
@@ -46,12 +62,32 @@ public class Drivetrain extends Subsystem {
 		}
 	}
 
+	@Override
 	public void initDefaultCommand() {
-		setDefaultCommand(new DriveByJoystick());
+		this.setDefaultCommand(new DriveByJoystick());
 	}
 
 	public void drive(double move, double rotate, double strafe) {
 		// Not Implemented
 	}
 
+	public void sidewinderDown() {
+		this.sidewinderModule.set(Value.kForward);
+	}
+
+	public void sidewinderUp() {
+		this.sidewinderModule.set(Value.kReverse);
+	}
+
+	public double getSpeedLeftFront() {
+		return this.leftFront.getSpeed();
+	}
+
+	public double getSpeedRightFront() {
+		return this.rightFront.getSpeed();
+	}
+
+	public double getSpeedSidewinder() {
+		return this.sidewinder.getSpeed();
+	}
 }
