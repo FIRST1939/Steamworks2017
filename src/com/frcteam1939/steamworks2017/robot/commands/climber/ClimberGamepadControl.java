9288005c6459f47ac95d9bda8e3a8ef1f0b8e0f5
learnSola <sdugbo@learnscienceandmathclub.org>
2017-01-28 11:08:21 -0600

    Added Climber
    Fixed Climber code added Encoders and and CANTalons

--- src/com/frcteam1939/steamworks2017/robot/commands/climber/ClimberGamepadControl.java
+++ src/com/frcteam1939/steamworks2017/robot/commands/climber/ClimberGamepadControl.java
@@ -23,7 +23,7 @@ public class ClimberGamepadControl extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return true;
+        return false;
     }
 
     // Called once after isFinished returns true
