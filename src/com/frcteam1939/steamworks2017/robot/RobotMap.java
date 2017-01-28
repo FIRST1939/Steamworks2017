e4f444d164f18dae1f706ba7fe87a1ed3d054ef2
azhawa <azhawa@hotmail.com>
2017-01-28 09:51:52 -0600

    Adding to the RobotMap

--- src/com/frcteam1939/steamworks2017/robot/RobotMap.java
+++ src/com/frcteam1939/steamworks2017/robot/RobotMap.java
@@ -10,5 +10,20 @@ public class RobotMap {
 	public static final int rightMidTalon = 0;
 	public static final int rightBackTalon = 0;
 	public static final int sidewinderTalon = 0;
+	public static final int climberTalon = 0;
+	public static final int fuelIntakeTalon = 0;
+	public static final int fuelOutputTalon = 0;
+	
+	// Solenoids
+	public static final int PCM = 0;
+	public static final int brakeDownSolenoid = 0;
+	public static final int fuelDoorOutSolenoid = 0;
+	public static final int gearRampOutSolenoid = 0;
+	public static final int gearPushSolenoid = 0;
+	public static final int sidewinderDownSolenoid = 0;
+	public static final int sidewinderUpSolenoid = 0;
+	
+	// DIO
+	public static final int fuelLineBreak = 0;
 	
 }
