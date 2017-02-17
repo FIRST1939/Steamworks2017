9179d33147ee39adb5b5f30147937227fb7dbc69
PCApple <poc.kcchiefs@gmail.com>
2017-02-16 20:41:30 -0600

    Added Vision Code, not tested

--- src/com/frcteam1939/steamworks2017/robot/Robot.java
+++ src/com/frcteam1939/steamworks2017/robot/Robot.java
@@ -1,6 +1,8 @@
 
 package com.frcteam1939.steamworks2017.robot;
 
+import org.opencv.core.Mat;
+
 import com.frcteam1939.steamworks2017.robot.subsystems.Brake;
 import com.frcteam1939.steamworks2017.robot.subsystems.Climber;
 import com.frcteam1939.steamworks2017.robot.subsystems.Drivetrain;
@@ -9,6 +11,10 @@ import com.frcteam1939.steamworks2017.robot.subsystems.FuelOutput;
 import com.frcteam1939.steamworks2017.robot.subsystems.GearIntake;
 import com.frcteam1939.steamworks2017.robot.subsystems.GearOutput;
 
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -24,6 +30,7 @@ public class Robot extends IterativeRobot {
 	public static final GearOutput gearOutput = new GearOutput();
 	public static final FuelIntake fuelIntake = new FuelIntake();
 	public static final FuelOutput fuelOutput = new FuelOutput();
+	public Pipeline pipe = new Pipeline();
 
 	public static OI oi;
 
@@ -35,6 +42,19 @@ public class Robot extends IterativeRobot {
 		oi = new OI();
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		// SmartDashboard.putData("Auto mode", chooser);
+		new Thread(() -> {
+            UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+            camera.setResolution(640, 480);
+            camera.setBrightness(10);
+            CvSink cvSink = CameraServer.getInstance().getVideo();
+            CvSource outputStream = CameraServer.getInstance().putVideo("Processed", 640, 480);
+            Mat source = new Mat();
+            while(!Thread.interrupted()) {
+                cvSink.grabFrame(source);
+                pipe.process(source);
+                outputStream.putFrame(pipe.rgbThresholdOutput());
+            }
+        }).start();
 	}
 
 	@Override
