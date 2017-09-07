eaba237652bcc42dedd46526e7a0c67da1512727
azhawa <azhawa@hotmail.com>
2017-09-07 16:42:47 -0500

    Setting FuelOutput and FuelIntake to Buttons

--- src/com/frcteam1939/steamworks2017/robot/OI.java
+++ src/com/frcteam1939/steamworks2017/robot/OI.java
@@ -5,8 +5,6 @@ import com.frcteam1939.steamworks2017.robot.commands.fuelintake.SetFuelIntakeSpe
 import com.frcteam1939.steamworks2017.robot.commands.fueloutput.SetFuelOutputSpeed;
 import com.frcteam1939.steamworks2017.robot.commands.gearoutput.PushOutGear;
 import com.frcteam1939.steamworks2017.robot.commands.gearoutput.RetractGearPusher;
-import com.frcteam1939.steamworks2017.robot.subsystems.FuelIntake;
-import com.frcteam1939.steamworks2017.robot.subsystems.FuelOutput;
 import com.frcteam1939.steamworks2017.util.Gamepad;
 import com.frcteam1939.steamworks2017.util.RunCode;
 
@@ -23,10 +21,10 @@ public class OI {
 	public OI() {
 		this.gamepad.y.whenPressed(new PushOutGear());
 		this.gamepad.b.whenPressed(new RetractGearPusher());
-		this.gamepad.rightTrigger.whenPressed(new SetFuelOutputSpeed(FuelOutput.OUT_SPEED));
-		this.gamepad.rightTrigger.whenReleased(new SetFuelOutputSpeed(0));
-		this.gamepad.leftTrigger.whenPressed(new SetFuelIntakeSpeed(FuelIntake.IN_SPEED));
-		this.gamepad.leftTrigger.whenReleased(new SetFuelIntakeSpeed(0));
+		// this.gamepad.rightTrigger.whenPressed(new SetFuelOutputSpeed(FuelOutput.OUT_SPEED));
+		this.gamepad.rightButton.whenReleased(new SetFuelOutputSpeed(0));
+		// this.gamepad.leftTrigger.whenPressed(new SetFuelIntakeSpeed(FuelIntake.IN_SPEED));
+		this.gamepad.leftButton.whenReleased(new SetFuelIntakeSpeed(0));
 
 		new JoystickButton(this.left, 2).whenPressed(new BrakeDown());
 		new JoystickButton(this.left, 3).whenPressed(new BrakeDown());
